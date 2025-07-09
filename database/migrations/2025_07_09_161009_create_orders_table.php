37c1a4e0c5682ca722e4f5ef8b4d3dd8d3d3a25b
saitki <85718309+saitki@users.noreply.github.com>
2025-07-09 11:41:54 -0500

    Add elements a migrations table order

--- database/migrations/2025_07_09_161009_create_orders_table.php
+++ database/migrations/2025_07_09_161009_create_orders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->date('fecha_pedido');
+            $table->enum('estado', ['pendiente', 'en_proceso', 'completed'])->default('pendiente');
             $table->timestamps();
         });
     }
