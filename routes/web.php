09565ceabdae07f76144f4ab5700787cd95ef141
saitki <85718309+saitki@users.noreply.github.com>
2025-07-09 15:18:31 -0500

    Add new route's for post, put and delete for order

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [OrderController::class, 'index'])->name('orders.index');
+Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
+Route::put('/orders/{order}', [OrderController::class, 'update'])->name('orders.update');
+Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
